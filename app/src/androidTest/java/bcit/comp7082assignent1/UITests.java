e9d58cc135d29656d1f5e08dd5074f756be2498b
Paul1005 <mccarliep@gmail.com>
2018-10-10 22:20:26 -0700

    updated uitests

--- app/src/androidTest/java/bcit/comp7082assignent1/UITests.java
+++ app/src/androidTest/java/bcit/comp7082assignent1/UITests.java
@@ -17,6 +17,7 @@ import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import static org.hamcrest.core.StringContains.containsString;
 
 @RunWith(AndroidJUnit4.class)
@@ -24,67 +25,86 @@ public class UITests {
     @Rule
     public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(MainActivity.class);
 
+    public String caption;
+    public String expectedCaption1;
+    public String expectedCaption2;
+    public String expectedCaption3;
+    public String maxDate;
+    public String minDate;
+    public String expectedDate1;
+    public String expectedDate2;
+    public String expectedDate3;
+    public Float latitude1;
+    public Float longitude1;
+    public Float latitude2;
+    public Float longitude2;
+
     @Before
-    public void beforeTest(){
-        
+    public void beforeTest() {
+        //onView(withId(R.id.btnCamera)).perform(click());
+        caption = "caption1";
+        expectedCaption1 = "caption1";
+        expectedCaption2 = "caption2";
+        expectedCaption3 = "caption3";
+        minDate = "20181010190926";
+        maxDate = "20181010190928";
+        expectedDate1 = "20181010_190905";
+        expectedDate2 = "20181010_190927";
+        expectedDate3 = "20181010_190917";
+        latitude1 = 49f;
+        longitude1 = -123f;
+        latitude1 = 1f;
+        longitude1 = 1f;
     }
 
     @Test
     public void TestCaptionFilter() {
-        onView(withId(R.id.caption)).perform(replaceText("test1"), closeSoftKeyboard());
-        onView(withId(R.id.btnRight)).perform(click());
-        onView(withId(R.id.btnLeft)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
         onView(withId(R.id.btnFilter)).perform(click());
-        onView(withId(R.id.search_caption)).perform(typeText("test1"), closeSoftKeyboard());
+        onView(withId(R.id.search_caption)).perform(typeText(caption), closeSoftKeyboard());
         onView(withId(R.id.search_search)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
-        for (int i = 0; i <= 5; i++) {
+        onView(withId(R.id.caption)).check(matches(withText(containsString(expectedCaption1))));
+        for (int i = 0; i <= 3; i++) {
             onView(withId(R.id.btnRight)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
+            onView(withId(R.id.caption)).check(matches(withText(containsString(expectedCaption1))));
             onView(withId(R.id.btnLeft)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
+            onView(withId(R.id.caption)).check(matches(withText(containsString(expectedCaption1))));
         }
     }
 
     @Test
     public void TestDateFilter() {
-        onView(withId(R.id.caption)).perform(replaceText("test1"), closeSoftKeyboard());
-        onView(withId(R.id.btnRight)).perform(click());
-        onView(withId(R.id.btnLeft)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
         onView(withId(R.id.btnFilter)).perform(click());
-        onView(withId(R.id.search_caption)).perform(typeText("test1"), closeSoftKeyboard());
+        onView(withId(R.id.search_fromDate)).perform(typeText(minDate), closeSoftKeyboard());
+        onView(withId(R.id.search_toDate)).perform(typeText(maxDate), closeSoftKeyboard());
         onView(withId(R.id.search_search)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
-        for (int i = 0; i <= 5; i++) {
+        onView(withId(R.id.timestamp)).check(matches(withText(containsString(expectedDate2))));
+        for (int i = 0; i <= 3; i++) {
             onView(withId(R.id.btnRight)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
+            onView(withId(R.id.caption)).check(matches(withText(containsString(expectedDate2))));
             onView(withId(R.id.btnLeft)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
+            onView(withId(R.id.caption)).check(matches(withText(containsString(expectedDate2))));
         }
     }
 
     @Test
     public void TestLocationFilter() {
-        onView(withId(R.id.caption)).perform(replaceText("test1"), closeSoftKeyboard());
-        onView(withId(R.id.btnRight)).perform(click());
-        onView(withId(R.id.btnLeft)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
         onView(withId(R.id.btnFilter)).perform(click());
-        onView(withId(R.id.search_caption)).perform(typeText("test1"), closeSoftKeyboard());
+        onView(withId(R.id.search_latitude)).perform(typeText(latitude1), closeSoftKeyboard());
+        onView(withId(R.id.search_longitude)).perform(typeText(longitude1), closeSoftKeyboard());
         onView(withId(R.id.search_search)).perform(click());
-        onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
-        for (int i = 0; i <= 5; i++) {
-            onView(withId(R.id.btnRight)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
-            onView(withId(R.id.btnLeft)).perform(click());
-            onView(withId(R.id.caption)).check(matches(withText(containsString("test1"))));
-        }
+        onView(withId(R.id.caption)).check(matches(withText(containsString("caption2"))));
+        onView(withId(R.id.btnRight)).perform(click());
+        onView(withId(R.id.caption)).check(matches(withText(containsString("caption3"))));
+        onView(withId(R.id.btnRight)).perform(click());
+        onView(withId(R.id.caption)).check(matches(withText(containsString("caption1"))));
+        onView(withId(R.id.btnFilter)).perform(click());
+        onView(withId(R.id.search_latitude)).perform(typeText(latitude2), closeSoftKeyboard());
+        onView(withId(R.id.search_longitude)).perform(typeText(longitude2), closeSoftKeyboard());
+        onView(withId(R.id.caption)).check(matches(withText(containsString(""))));
     }
 
     @After
-    public void afterTest(){
+    public void afterTest() {
 
     }
 }
